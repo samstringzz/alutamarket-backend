8d19d4ed867fadde561f99fc6cdbb66b51446433
Foley13 <folajimiopeyemisax13@gmail.com>
2024-07-01 08:39:07 +0100

    ...

--- server.go
+++ server.go
@@ -75,10 +75,10 @@ func PSWebhookHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	// Create a new CORS middleware with the desired options
 	c := cors.New(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:5173", "http://127.0.0.1:5173", "https://www.thealutamarket.com", "http:/www.thealutamarket.com"}, // Specify the allowed origins
-		AllowedMethods:   []string{"GET", "POST", "OPTIONS"},                                                                                           // Specify allowed HTTP methods
-		AllowedHeaders:   []string{"Authorization", "Content-Type"},                                                                                    // Specify allowed headers
-		AllowCredentials: true,                                                                                                                         // Allow credentials like cookies
+		AllowedOrigins:   []string{"http://localhost:5173", "http://127.0.0.1:5173", "https://www.thealutamarket.com", "https:/thealutamarket.com"}, // Specify the allowed origins
+		AllowedMethods:   []string{"GET", "POST", "OPTIONS"},                                                                                        // Specify allowed HTTP methods
+		AllowedHeaders:   []string{"Authorization", "Content-Type"},                                                                                 // Specify allowed headers
+		AllowCredentials: true,                                                                                                                      // Allow credentials like cookies
 	})
 
 	// Load environment variables from .env file
