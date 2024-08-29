267624ac4b51ec17b4fe5c15224969fb5af95dfe
Foley13 <folajimiopeyemisax13@gmail.com>
2024-08-29 08:49:57 +0100

    ...

--- internals/store/store.go
+++ internals/store/store.go
@@ -82,15 +82,15 @@ type Order struct {
 	CartID          uint32           `json:"cart_id" db:"cart_id"`
 	Coupon          string           `json:"coupon,omitempty" db:"coupon"`
 	Fee             float64          `json:"fee" db:"fee"`
-	Status          string           `json:"status" db:"status"` //open || closed
+	Status          string           `json:"status" db:"status"` //order status
 	UserID          string           `json:"user_id" db:"user_id"`
 	DeliveryDetails DeliveryDetails  `gorm:"serializer:json" json:"delivery_details" db:"delivery_details"`
 	Amount          float64          `json:"amount" db:"amount"`
 	UUID            string           `json:"uuid" db:"uuid"`
 	PaymentGateway  string           `json:"payment_gateway" db:"payment_gateway"`
 	PaymentMethod   string           `json:"payment_method" db:"payment_method"`
-	TextRef         string           `json:"txt_ref" db:"txt_ref"`
-	TxtStatus       string           `json:"txt_status" db:"txt_status"`
+	TransRef        string           `json:"trt_ref" db:"trt_ref"`
+	TransStatus     string           `json:"txt_status" db:"txt_status"` //Transaction status
 	Products        []TrackedProduct `gorm:"serializer:json" json:"products" db:"products"`
 	CreatedAt       time.Time        `json:"created_at" db:"created_at"`
 	UpdatedAt       time.Time        `json:"updated_at" db:"updated_at"`
