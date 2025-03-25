def4102aa05ff624583ddab0ecdbe98b2cf77c92
samstringz <freetlessguitarist@gmail.com>
2025-03-25 16:37:32 +0100

    env update

--- internals/user/user_repository.go
+++ internals/user/user_repository.go
@@ -66,11 +66,12 @@ func NewRepository() Repository {
 	dbURL := os.Getenv("DATABASE_URL")
 	if dbURL == "" {
 		log.Printf("DATABASE_URL not found, using individual connection parameters")
-		dbURL = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=require",
+		password := strings.ReplaceAll(os.Getenv("DB_PASSWORD"), "#", "%23")
+		dbURL = fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=require",
+			os.Getenv("DB_USER"),
+			password,
 			os.Getenv("DB_HOST"),
 			os.Getenv("DB_PORT"),
-			os.Getenv("DB_USER"),
-			os.Getenv("DB_PASSWORD"),
 			os.Getenv("DB_NAME"),
 		)
 	}
