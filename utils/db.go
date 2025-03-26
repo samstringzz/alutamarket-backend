39b633fdf04d3f1f7d736691c0022617700682ba
samstringz <freetlessguitarist@gmail.com>
2025-03-26 16:00:59 +0100

    optimization

--- utils/db.go
+++ utils/db.go
@@ -1,11 +1,13 @@
 package utils
 
 import (
-	"gorm.io/gorm"
 	"log"
 	"time"
+
+	"gorm.io/gorm"
 )
 
+// RetryConnection attempts to establish a database connection with retries
 func RetryConnection(db *gorm.DB, maxRetries int) error {
 	var err error
 	for i := 0; i < maxRetries; i++ {
@@ -25,4 +27,28 @@ func RetryConnection(db *gorm.DB, maxRetries int) error {
 		time.Sleep(time.Second * time.Duration(i+1))
 	}
 	return err
+}
+
+// ConfigureConnectionPool sets up the database connection pool with recommended settings
+func ConfigureConnectionPool(db *gorm.DB) error {
+	sqlDB, err := db.DB()
+	if err != nil {
+		return err
+	}
+
+	sqlDB.SetMaxIdleConns(10)
+	sqlDB.SetMaxOpenConns(50)
+	sqlDB.SetConnMaxLifetime(time.Hour)
+	sqlDB.SetConnMaxIdleTime(30 * time.Minute)
+
+	return nil
+}
+
+// CheckConnection verifies if the database connection is alive
+func CheckConnection(db *gorm.DB) error {
+	sqlDB, err := db.DB()
+	if err != nil {
+		return err
+	}
+	return sqlDB.Ping()
 }
